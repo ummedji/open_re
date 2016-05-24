ca1608cdfcfc5a59eb48beddf98945163bc65d1f
chirag-ardeshna <ardeshnachirag@gmail.com>
2016-05-24 12:05:01 +0530

    adding modules

--- public/themes/default/header.php
+++ public/themes/default/header.php
@@ -1,15 +1,3 @@
-<?php
-
-Assets::add_css(array('bootstrap.min.css', 'style.css','green.css','hover.css','jquery.mCustomScrollbar.css',
-    'bootstrap-select.min.css','bootstrap-datepicker.min.css'));
-
-//Assets::add_js('bootstrap.min.js');
-
-$inline  = '$(".dropdown-toggle").dropdown();';
-$inline .= '$(".tooltips").tooltip();';
-Assets::add_js($inline, 'inline');
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,12 +9,25 @@ Assets::add_js($inline, 'inline');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php e(isset($meta_description) ? $meta_description : ''); ?>">
     <meta name="author" content="<?php e(isset($meta_author) ? $meta_author : ''); ?>">
+
+    <?php
+    Assets::add_css(array('bootstrap.min.css', 'style.css','green.css','hover.css','jquery.mCustomScrollbar.css',
+        'bootstrap-select.min.css','bootstrap-datepicker.min.css'));
+    //Assets::add_js('bootstrap.min.js');
+    $inline  = '$(".dropdown-toggle").dropdown();';
+    $inline .= '$(".tooltips").tooltip();';
+    Assets::add_js($inline, 'inline');
+    ?>
+
     <!-- Custom Fonts -->
     <link href="<?php echo Template::theme_url('font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+
+
     <?php echo Assets::css(); ?>
     <link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico">
     <script src="<?php echo Template::theme_url('js/jquery.min.js'); ?>"></script>
